86c25abb14d7b5f1065ae368ad6970a807d801eb
Stevan Dupor <smdupor@gmail.com>
2022-02-06 13:08:35 -0500

    Update with enumeration

--- project_benches/lab_1/testbench/top.sv
+++ project_benches/lab_1/testbench/top.sv
@@ -27,6 +27,10 @@ module top();
 	logic [WB_ADDR_WIDTH-1:0] adr_in;
 	logic [WB_DATA_WIDTH-1:0] dat_in;
 	logic we_in;
+	
+	enum logic[7:0] {ENABLE_CORE_INTERRUPT=8'b11xxxxxx, SET_I2C_BUS=8'bxxxxx110, I2C_START=8'bxxxxx100, 
+						I2C_WRITE=8'bxxxxx001, I2C_STOP=8'bxxxxx101} cmd;
+	enum bit [2:0] {CSR=2'b00, DPR=2'b01, CMDR=2'b10} dut_reg;
 
 	// FIRE INITIAL LOGIC BLOCKS
 	initial clock_generator();
@@ -80,60 +84,60 @@ module top();
 
 		$display("STARTING TEST FLOW");
 
-		#1000 wb_bus.master_write(2'b00, 8'b11xxxxxx); 	// Enable core by writing to CSR (EXAMPLE 1)
+		#1000 wb_bus.master_write(CSR, ENABLE_CORE_INTERRUPT); 	// Enable core by writing to CSR (EXAMPLE 1)
 
-		wb_bus.master_write(2'b01, 8'h00); 				// 005 to DPR (EXAMPLE 3 STEP 1)
-		wb_bus.master_write(2'h2, 8'bxxxxx110);		 	// 110 to cmdr STEP 2
+		wb_bus.master_write(DPR, 8'h00); 				// 005 to DPR (EXAMPLE 3 STEP 1)
+		wb_bus.master_write(CMDR, SET_I2C_BUS);		 	// 110 to cmdr STEP 2
 
 		wait(irq==1'b1); 								// STEP 3
-		wb_bus.master_read(2'h2, adr_in);
+		wb_bus.master_read(CMDR, adr_in);
 		@(posedge clk);
 
-		wb_bus.master_write(2'h2, 8'bxxxxx100); 		//100 to cmdr STEP 4
+		wb_bus.master_write(CMDR, I2C_START); 		//100 to cmdr STEP 4
 		
 		wait(irq==1'b1); 								// STEP 5
-		wb_bus.master_read(2'h2, adr_in);
+		wb_bus.master_read(CMDR, adr_in);
 		@(posedge clk);
 
-		wb_bus.master_write(2'b01, 8'h44); 				//44 (SLAVE ADDR) to dpr STEP 6
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 7
+		wb_bus.master_write(DPR, 8'h44); 				//44 (SLAVE ADDR) to dpr STEP 6
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 7
 		
 		wait(irq==1'b1); 								// STEP 8
-		wb_bus.master_read(2'h2, adr_in);	////TODO: The NACK bit needs to be checked here; if we receive a NACK, need to stop.
+		wb_bus.master_read(CMDR, adr_in);	////TODO: The NACK bit needs to be checked here; if we receive a NACK, need to stop.
 		@(posedge clk);
 
-		wb_bus.master_write(2'b01, 8'hde); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hde); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		
 		wait_interrupt();
 		
-		wb_bus.master_write(2'b01, 8'had); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'had); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 	 	// WRITE Command STEP 10
 		wait_interrupt();
-		wb_bus.master_write(2'b01, 8'hbe); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hbe); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		wait_interrupt();
-		wb_bus.master_write(2'b01, 8'hef); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hef); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		wait_interrupt();
-		wb_bus.master_write(2'b01, 8'hfe); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hfe); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		wait_interrupt();
-		wb_bus.master_write(2'b01, 8'hed); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hed); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		wait_interrupt();
-		wb_bus.master_write(2'b01, 8'hbe); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hbe); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		wait_interrupt();
-		wb_bus.master_write(2'b01, 8'hef); 				//78 (DATA) to dpr STEP 9
-		wb_bus.master_write(2'h2, 8'bxxxxx001); 		// WRITE Command STEP 10
+		wb_bus.master_write(DPR, 8'hef); 				//78 (DATA) to dpr STEP 9
+		wb_bus.master_write(CMDR, I2C_WRITE); 		// WRITE Command STEP 10
 		wait_interrupt();
 
 
-		wb_bus.master_write(2'h2, 8'bxxxxx101); 		// STOP Command STEP 12
+		wb_bus.master_write(CMDR, I2C_STOP); 		// STOP Command STEP 12
 	
 		wait(irq==1'b1); 								// STEP 13	
-		wb_bus.master_read(2'h2, adr_in);
+		wb_bus.master_read(CMDR, adr_in);
 		
 		$display("TASK DONE");
 
@@ -144,6 +148,11 @@ task wait_interrupt();
 	wait(irq==1'b1); 								// STEP 11
 	wb_bus.master_read(2'h2, adr_in);
 	@(posedge clk);
+endtask
+
+task select_I2C_bus(input bit [7:0] i);
+
+	
 endtask
 
 	// ****************************************************************************
