d9308f00ebfbdc9c6f392b99316ed3ec83eef2a8
Stevan Dupor <smdupor@gmail.com>
2022-03-03 11:31:08 -0500

    Update makefiles to compile all new files

---
+++ verification_ip/environment_packages/i2cmb_env_pkg/i2cmb_env_pkg.sv
@@ -0,0 +1,11 @@
+package i2cmb_env_pkg;
+
+`include "src/i2cmb_coverage.svh"
+`include "src/i2cmb_env_configuration.svh"
+`include "src/i2cmb_generator.svh"
+`include "src/i2cmb_predictor.svh"
+`include "src/i2cmb_scoreboard.svh"
+`include "src/i2cmb_test.svh"
+
+
+endpackage
