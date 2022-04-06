de3595025fee8c87dd5c7630b374173a48cf67d6
Stevan Dupor <smdupor@gmail.com>
2022-04-05 23:02:06 -0400

    Add assertions to match testplan and updated xml file

--- verification_ip/interface_packages/wb_pkg/src/wb_monitor.svh
+++ verification_ip/interface_packages/wb_pkg/src/wb_monitor.svh
@@ -58,12 +58,50 @@ class wb_monitor extends ncsu_component#(.T(wb_transaction));
 		static T temp;
 		temp =new;
 		if(last_trans[0] != null)
-		if(last_trans[0].line == CMDR && last_trans[0].word[2:0] == M_I2C_START && monitored_trans.write == I2_READ) begin
+		if(last_trans[0].line == CMDR && monitored_trans.write == I2_READ) begin 	//	The last transaction was a command, and we are clearing the interrupt
 				this.bus.master_read(STATE, temp.word);
-
-				assert_fsm_byte_match_last_cmd: assert (temp.word[2:0]==monitored_trans.word[2:0])
+				assert_fsm_byte_match_last_cmd: assert (temp.word[6:4]==last_trans.word[2:0])		// FSM Byte Command Match 
 				else $error("Assertion assert_fsm_byte_match failed!");
+				
+				assert_done_raised_on_complete: assert (monitored_trans.word[7]==1'b1);				// Done Bit was raised signaling complete
+				else $error("Assertion assert_done_raised_on_complete failed!");
+
+				if(last_trans[0].word == SET_I2C_BUS) begin
+					this.bus.master_read(CSR, temp.word);
+					assert_bus_id_match: assert(temp.word[3:0]==last_trans[1].word[3:0]);			// Captured Bus matches selected bus
+					else $error("Assertion assert_bus_id_match failed!");
+
+					assert_bc_on_bus_capture: assert(temp.word[4]==1'b1);							// Bus capture bit raised on capture
+					else $error("Assertion assert_bc_on_bus_capture failed!");
+				end 
+				else if (last_trans[0].word == I2C_START || last_trans[0].word == I2C_WRITE || last_trans[0].word == READ_WITH_ACK ) begin
+					this.bus.master_read(CSR, temp.word);
+					assert_bb_during_transaction: assert(temp.word[5]==1'b1);						// Bus Busy during transaction
+					else $error("Assertion assert_bb_during_transaction failed!");
+				end
 		end
+
+		// Check register default values on a DUT-enable
+		else if (monitored_trans.line == CSR && monitored_trans.word[7]==1'b1 && monitored_trans.write == I2_WRITE) begin
+			this.bus.master_read(CSR, temp.word);												//CSR Defaults
+			assert_csr_enable_defaults: assert(temp.word[7:6] == monitored_trans[7:6]);
+			else $error("Assertion csr_intr_disable!");
+
+			this.bus.master_read(DPR, temp.word);												// DPR Default
+			assert_dpr_default_on_enable: assert(temp.word == 8'b0);
+			else $error("Assertion assert_dpr_default_on_enable failed!");
+
+			this.bus.master_read(CMDR, temp.word);
+			assert_cmdr_default_on_enable: assert(temp.word == 8'b1000_0000);					// CMDR Default
+			else $error("Assertion assert_cmdr_default_on_enable failed!");
+
+			this.bus.master_read(STATE, temp.word);
+			assert_fsmr_default_on_enable: assert(temp.word == 8'h0);
+			else $error("Assertion assert_fsmr_default_on_enable failed!");
+
+		end
+
+
 
 	endtask
 	/*
